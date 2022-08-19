fa6af120bb1901815c04f8b525f500974c542ba2
JoBaHP <jocoraos@yahoo.com>
2022-08-19 17:10:00 +0200

    added numeric filtering

--- controllers/meals.js
+++ controllers/meals.js
@@ -1,30 +1,68 @@
+import Meal from "../models/meal.js";
+
+const getAllMealsStatic = async (req, res) => {
+  const meals = await Meal.find({ price: { $gt: 30 } })
+    .sort("price")
+    .select("name price");
+
+  res.status(200).json({ meals, nbHits: meals.length });
+};
 const getAllMeals = async (req, res) => {
-  const { featured, group, name, sort, fields, numericFilters } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
+
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
   }
-  if (group) {
-    queryObject.group = group;
+  if (company) {
+    queryObject.company = company;
   }
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
   }
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
+  }
+
   let result = Meal.find(queryObject);
+  // sortprod
   if (sort) {
     const sortList = sort.split(",").join(" ");
-    result - result.sort(sortList);
+    result = result.sort(sortList);
   } else {
     result = result.sort("createdAt");
   }
+
   if (fields) {
     const fieldsList = fields.split(",").join(" ");
     result = result.select(fieldsList);
   }
-  const page = Nubmer(req.query.page) || 1;
-  const limit = Number(req.query.limit) || 9;
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
   const skip = (page - 1) * limit;
+
   result = result.skip(skip).limit(limit);
+  // 23
+  // 4 7 7 7 2
+
   const meals = await result;
   res.status(200).json({ meals, nbHits: meals.length });
 };
